ec05f03dd6f70fcef731ab477033a332fa09610c
JordanNitta <Jordannittarodrigues@gmail.com>
2023-01-17 10:04:42 -0800

    Second Commit

--- playground/server.py
+++ playground/server.py
@@ -3,22 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def load_page():
-    return "User http://127.0.0.1:5001/play or http://127.0.0.1:5001/play/10 or any number of boxes you want it to display"
+    return render_template("home.html")
 
 @app.route('/play')
 def play():
     return render_template("playground.html")
 
 
-@app.route('/play/<boxes>')
+@app.route('/play/<int:boxes>')
 def play_box(boxes):
-    repeat_boxes = int(boxes)
-    return render_template("playground1.html", repeat_boxes=repeat_boxes)
+    return render_template("playground1.html", repeat_boxes=boxes)
 
-@app.route('/play/<boxes>/<color>')
+@app.route('/play/<int:boxes>/<string:color>')
 def play_box_3(boxes, color):
-    repeat_boxes = int(boxes)
-    return render_template("playground2.html", repeat_boxes=repeat_boxes, color=color)
+    return render_template("playground2.html", repeat_boxes=boxes, color=color)
 
 if __name__=="__main__":
     app.run(debug=True, port=5001)
